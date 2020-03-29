2c13b7a6462f62591a54909be6440fcc570702ef
harinee007 <harineek7@gmail.com>
2020-03-29 07:05:10 +0530

    First Commit

---
+++ src/Amazon1.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Amazon1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\TCS OE\\ELECTIVE 3\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		String url="https://www.amazon.in/";
+		driver.get(url);
+		Thread.sleep(5000);
+		
+		/*Actions action=new Actions(driver);
+		WebElement hover=driver.findElement(By.className("nav-line-4"));
+		action.moveToElement(hover).perform();
+		System.out.println("Hovered to sign in");*/
+	}
+
+}
